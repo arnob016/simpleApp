305660a34f8f9ecee78f912e3302ea18fcc6f74e
arnob016 <arnobdey008@gmail.com>
2023-05-30 20:13:27 +0600

    Upload

---
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin, btnSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnUserLogin);
+        btnSignUp = findViewById(R.id.btnUserSignup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
